62bc88bbe9c34e390994b8b45ac5f212bf497c49
Pablo Nuñez Pölcher <polcher@amazon.com>
2023-04-19 14:00:13 +0000

    Bootstrap script. Requirements for different PyTorch versions.

---
+++ bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+install_dependencies () {
+  python3 -m venv .venv
+  . .venv/bin/activate
+  pip install -U pip
+  pip install -r requirements.txt
+  deactivate
+}
+
+# Install dependencies
+sudo apt install python3.10-venv
+
+# Initialize PyTorch 1.13 venv
+cd pytorch
+install_dependencies
+cd ..
+
+# Initialize PyTorch 2.0 venv
+cd pytorch2
+install_dependencies
+cd..
+
